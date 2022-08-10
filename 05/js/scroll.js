0c9547826d64f88797163d50677109a999906e8d
Ael <bluebayband@gmail.com>
2022-08-10 08:36:15 -0700

    everything except for footer

--- 05/js/scroll.js
+++ 05/js/scroll.js
@@ -1,5 +1,19 @@
 const headerClassList = document.querySelector("header").classList;
+const spanClassList = document.querySelector("header nav span:last-child").classList;
+const span = document.querySelector("header nav span:nth-child(2)");
 
-window.addEventListener("scroll", () => {
-    window.pageYOffset >= 100 ? headerClassList.add("shadow-box") : headerClassList.remove("shadow-box");
-})
+
+const onScroll = () => {
+    if (window.pageYOffset >= 100) {
+        headerClassList.add("shadow-box");
+        spanClassList.add("hidden");
+        span.innerText = "Schwartz Media";
+
+    } else {
+        headerClassList.remove("shadow-box");
+        spanClassList.remove("hidden");
+        span.innerText = "Schwartz";
+    }
+}
+
+window.addEventListener("scroll", onScroll);
